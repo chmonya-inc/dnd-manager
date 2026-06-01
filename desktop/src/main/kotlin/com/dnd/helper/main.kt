7ecec4cfe0eeb16226fc861640f577c53afe634b
JumpyWiz <tima.klokov@gmail.com>
2026-06-01 14:08:46 +0300

    new icons

--- desktop/src/main/kotlin/com/dnd/helper/main.kt
+++ desktop/src/main/kotlin/com/dnd/helper/main.kt
@@ -1,5 +1,6 @@
 package com.dnd.helper
 
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
@@ -12,6 +13,7 @@ fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "D&D Helper",
+        icon = painterResource("icon.png"),
         state = windowState
     ) {
         DndHelperTheme {
